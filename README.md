d430c76ac7a62e95f39afd22a7c7a5ca908c9581
Brian Felix <fbrian100@gmail.com>
2022-07-19 16:56:47 -0400

    It's an update
    - Linting
    - Archive photos into a 'used' folder after Tweeting
    - Add randomEmoji function

--- README.md
+++ README.md
@@ -3,14 +3,17 @@ Cobalt is an agency operating as a collective. More soon...
 
 ## Purpose
 
-A study on aethetics. We've curated the images in the folder and are leaning on the Twitter audience to curate by 'liking' their favorites. If you want to create your own, clone this repo and follow the quickstart below.
+A study on aethetics. It is a collection of found content and knowledge, that is relevant for my research, which spans the concept of wabi-sabi, inflatable objects, and at the intersection on the constructivism and organicism movements. We've curated the images in the folder and are leaning on the Twitter audience to curate by 'liking' their favorites. 
+
+If you want to create your own, clone this repo and follow the quickstart below.
 
 ## Quickstart
 
 1. Run `npm install` in the terminal to install dependendencies (`cron`, `dotenv`, and `twit`)
 2. Add your Twitter API credentials to `config.js`
-4. Create a folder called `images` -- This is where you will store your images
+4. Create a folder called `images` and inside of that folder, another called `used`-- You will store your images ins the `images` folder and they will get programatically archived in the `used` folder.
 5. Run `node server.js` in the terminal
 
 ### Roadmap
 - Pinterest and Are.na integrations
+- ~~Programatically move photos that were already tweeted to a different folder~~
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const fs = require( 'fs' ),
+const fs = require( 'fs-extra' ),
       path = require( 'path' ),
       Twit = require( 'twit' ),
       CronJob = require ( 'cron' ).CronJob,
@@ -9,67 +9,74 @@ dotenv.config()
 
 const T = new Twit( config );
 
-var emojiArray = ['💙', '🥶', '🦋', '🧢', '🚙', '🔵', '👖', '👕', '🔹', '🌀', '🧵', '🟦', '📘', '🏧']; // add whatever text you want here
-let randomEmoji = emojiArray[Math.floor(Math.random() * emojiArray.length)];
+function randomEmoji(){
+  var emojiArray = ['💙', '🥶', '🦋', '🧢', '🚙', '🔵', '👖', '👕', '🔹', '🌀', '🧵', '🟦', '📘', '🏧']; // add whatever text you want here
+  return emojiArray[Math.floor(Math.random() * emojiArray.length)];
+}
 
-function randomFromArray( images ){
-    return images[Math.floor( Math.random() * images.length )];
+function randomFromArray(images){
+  return images[Math.floor(Math.random() * images.length)];
 }
 
 function tweetRandomImage(){
-    fs.readdir( __dirname + '/images', function( err, files ) {
-      if ( err ){
-        console.log( 'error:', err );
-      }
-      else{
-        let images = [];
-        files.forEach( function( f ) {
-          images.push( f );
-        } );
-  
-        console.log( 'opening an image...' );
-  
-        const imagePath = path.join( __dirname, '/images/' + randomFromArray( images ) ),
-            b64content = fs.readFileSync( imagePath, { encoding: 'base64' } );
-  
-        console.log( 'uploading an image...' );
-  
-        T.post( 'media/upload', { 
-            media_data: b64content
-         }, function ( err, data, response ) {
-          if ( err ){
-            console.log( 'error:', err );
-          }
-          else{
-            const image = data;
-            console.log( 'image uploaded, adding description...' );
-  
-            T.post( 'media/metadata/create', {
-                media_id: image.media_id_string,
-                alt_text: {
-                    text: ''
-                }            
-            }, function( err, data, response ){
-              console.log( 'tweeting the image...' ); 
-              
-              T.post( 'statuses/update', {
-                status: randomEmoji,
-                media_ids: [image.media_id_string]
-              },
-                function( err, data, response) {
-                  if (err){
-                    console.log( 'error:', err );
-                  }
-                  else{
-                    console.log( 'posted an image!' );
-                  }
+  fs.readdir( __dirname + '/images', function( err, files ) {
+    if ( err ){
+      console.log( 'error:', err );
+    }
+    else {
+      let images = [];
+      files.forEach( function( f ) {
+        images.push( f );
+      } );
+
+      console.log( 'opening an image...' );
+      var selectedImage = randomFromArray(images)
+      const imagePath = path.join( __dirname, '/images/' + selectedImage ),
+        b64content = fs.readFileSync( imagePath, { encoding: 'base64' } );
+
+      console.log( 'uploading an image...' );
+
+      T.post( 'media/upload', { 
+        media_data: b64content
+        }, function ( err, data, response ) {
+        if ( err ){
+          console.log( 'error:', err );
+        }
+        else {
+          const image = data;
+          console.log( 'image uploaded, adding description...' );
+
+          T.post( 'media/metadata/create', {
+            media_id: image.media_id_string,
+            alt_text: {
+              text: ''
+            }            
+          }, function( err, data, response ){
+            console.log( 'tweeting the image...' ); 
+            
+            T.post( 'statuses/update', {
+              status: randomEmoji(),
+              media_ids: [image.media_id_string]
+            },
+              function( err, data, response) {
+                if (err){
+                  console.log( 'error:', err );
                 }
-              );
-            } );
-          }
-        } );      
-      }
-    } );
+                else {
+                  console.log( 'posted an image!' );
+
+                  fs.move(`./images/${selectedImage}`, `./images/used/${selectedImage}`, err => {
+                    if (err) return console.error(err)
+                    console.log('image archived!')
+                  })
+                }
+              }
+            );
+          } );
+        }
+      });      
+    }
+  });
 }
 
 const job = new CronJob("27,58 * * * *", () => {
